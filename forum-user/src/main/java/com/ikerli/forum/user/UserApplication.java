3e9a1b8ab6552b8f006de120ad3114f953e515d9
ikerli(李志勇) <ikerli@tencent.com>
2024-04-28 15:40:43 +0800

    first init

---
+++ forum-user/src/main/java/com/ikerli/forum/user/UserApplication.java
@@ -0,0 +1,17 @@
+package com.ikerli.forum.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @className: UserApplication
+ * @author: ikerli
+ * @description: TODO
+ * @createTime: 2024/4/28 15:08
+ */
+@SpringBootApplication
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+}
